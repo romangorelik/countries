c25816233723c0d1587cc7e6c185fd8e683c210a
Roman Gorelik <romang31@gmail.com>
2019-04-15 18:08:28 -0400

    Added button animation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
-import { images } from './helpers/backgroundImages.js'
+import { images } from './helpers/backgroundImages.js';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   image: string = images[0];
 
   constructor () {
     this.image = images[Math.floor(Math.random() * images.length)]
   }
+
+  ngOnInit () {
+    
+  }
+  
 }
--- src/app/helpers/backgroundImages.js
+++ src/app/helpers/backgroundImages.js
@@ -2,7 +2,7 @@ let images = [
   'https://images.unsplash.com/photo-1483728642387-6c3bdd6c93e5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2855&q=80',
   'https://images.unsplash.com/photo-1507525428034-b723cf961d3e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2853&q=80',
   'https://images.unsplash.com/photo-1442120108414-42e7ea50d0b5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2018&q=80',
-  'https://images.unsplash.com/photo-1488197047962-b48492212cda?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1947&q=80',
+  'https://images.unsplash.com/photo-1535190823090-3c159ed10a97?ixlib=rb-1.2.1&auto=format&fit=crop&w=1789&q=80',
   'https://images.unsplash.com/photo-1529963183134-61a90db47eaf?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2850&q=80'
 ];
 
