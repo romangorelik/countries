3a2f997c90f59e921d45e6043c77d51a777d2f29
Roman Gorelik <romang31@gmail.com>
2019-04-15 16:51:27 -0400

    First Commit. Added background images

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
 
+import { images } from './helpers/backgroundImages.js'
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Countries';
+  image: string = images[0];
+
+  constructor () {
+    this.image = images[Math.floor(Math.random() * images.length)]
+  }
 }
