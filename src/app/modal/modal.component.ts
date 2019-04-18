eb768be08156f2e52db0cfc479e28159b6c59085
Roman Gorelik <romang31@gmail.com>
2019-04-17 23:26:09 -0400

    Added buttons for time amount

--- src/app/modal/modal.component.ts
+++ src/app/modal/modal.component.ts
@@ -10,13 +10,31 @@ export class ModalComponent implements OnInit {
   wife: boolean = false;
   family: boolean = false;
   friends: boolean = false;
+  partyText:string = '';
+  buttonStates:any[] = [false, false, false];
 
   constructor() { }
 
   ngOnInit() {
   }
 
-  setAlone(alone, wife, family, friends) {
+  setAmountOfTime(one, two, three) {
+    this.buttonStates[0] = one;
+    this.buttonStates[1] = two;
+    this.buttonStates[2] = three;
+  }
+
+  setParty(alone, wife, family, friends) {
+    if (alone) {
+      this.partyText = 'You are going alone!'
+    } else if (wife) {
+      this.partyText = 'You are going on a romantic getaway!'
+    } else if (family) {
+      this.partyText = 'You are going on a family vacation!'
+    } else {
+      this.partyText = 'You are going away with friends!'
+    }
+
     this.alone = alone;
     this.wife = wife;
     this.family = family;
